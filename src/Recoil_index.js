ac2db5d3725173f8cbb64b43525b6db94214a801
jonthomp-hx <jon.thompson@hyperexponential.com>
2020-05-29 16:41:28 +0100

    Add build to repo

--- src/Recoil_index.js
+++ src/Recoil_index.js
@@ -68,6 +68,10 @@ const {
   waitForNone,
 } = require('./recoil_values/Recoil_WaitFor');
 
+const cacheMostRecent = require('./caches/Recoil_cacheMostRecent');
+const cacheWithReferenceEquality = require('./caches/Recoil_cacheWithReferenceEquality');
+const cacheWithValueEquality = require('./caches/Recoil_cacheWithValueEquality');
+
 module.exports = {
   // Types
   DefaultValue,
@@ -112,4 +116,9 @@ module.exports = {
 
   // Other functions
   isRecoilValue,
+
+  // Caches
+  cacheMostRecent,
+  cacheWithReferenceEquality,
+  cacheWithValueEquality,
 };
